cddd11120238b4604bf1fb26cbbced7a2a17d6ea
z3to <6gqrbj@posteo.org>
2020-02-18 15:59:18 +0100

    Settings 📟 added

--- dist.js
+++ dist.js
@@ -3,10 +3,14 @@ import rimraf from 'rimraf'
 import archiver from 'archiver'
 import chalk from 'chalk'
 import prettyBytes from 'pretty-bytes'
+import path from 'path'
 
 rimraf.sync('./dist')
 fs.mkdirSync('./dist')
 
+const TARGET_PATH = `dist`
+
+// Modules
 const modules = fs.readdirSync('./modules').filter(m => fs.lstatSync(`./modules/${m}`).isDirectory())
 
 modules.forEach(m => {
@@ -22,4 +26,16 @@ modules.forEach(m => {
     throw err
   })
   archive.finalize()
+})
+
+// Settings
+const files = fs.readdirSync('./settings').filter(m => path.extname(`${m}`) === '.json')
+
+files.forEach(f => {
+  const from = `./settings/${f}`
+  const to = `./${TARGET_PATH}/${f}`
+  fs.copyFile(from, to, (err) => {
+    if (err) throw err;
+    console.log(`${chalk.cyan.bold(from)} ${chalk.yellow.bold('→')} ${chalk.magenta.bold(to)} 📦`)
+  });
 })
